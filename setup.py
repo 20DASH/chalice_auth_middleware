ddd98682f67bc19074de7b1bac3d46b802590d5d
Fabio AM <fabio@20dash.com>
2024-10-24 17:24:30 -0300

    bugfix: NameError: name 'long_description' is not defined

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="chalice_auth_middleware",
     version="0.1.0",
